6289dbc53573a8274ab9257543ab433c65d369c4
Fatma YILDIZ <ftmyildizaydogdu@gmail.com>
2023-04-05 14:57:31 +0300

    Add files via upload

---
+++ day6/constants/globalConstants.py
@@ -0,0 +1,8 @@
+
+URL="https://www.saucedemo.com/"
+invalidlogin_errmsg ="Epic sadface: Username and password do not match any user in this service"
+required_username="Epic sadface: Username is required"
+required_password="Epic sadface: Password is required"
+locked_user="Epic sadface: Sorry, this user has been locked out."
+
+
